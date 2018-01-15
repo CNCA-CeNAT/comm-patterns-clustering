68ac7ce4aacf4f7c89eec73dd0545f1f5168fa43
diegorsjv <djimenez@cenat.ac.cr>
2018-01-15 14:43:24 -0600

    Added several statistics measurement to readAndProcessGraph.py, missing: scale freeness, characteristic path length

--- Scripts/readAndProcessGraph.py
+++ Scripts/readAndProcessGraph.py
@@ -5,6 +5,7 @@ from igraph import Graph
 from igraph import plot
 from igraph import layout
 import igraph as igraph 
+import networkx as nx
 import sys
 import math
 import matplotlib.pyplot as plt
@@ -22,6 +23,14 @@ graph = None
 vertexAdded = {}
 edges = {}
 
+
+def convertGraph(graphInput):
+	netXGraph = nx.Graph()
+	names = list(map(int, graphInput.vs['name']))	
+	netXGraph.add_nodes_from(names)
+	netXGraph.add_edges_from(graphInput.get_edgelist())	
+	#print(list(netXGraph.edges()))
+	return netXGraph
 
 def plotGraph(graph, outputDir):
 	fileName = outputDir + "/graph.png"
@@ -44,7 +53,56 @@ def removeOutliers(distribution):
 			continue
 	distribution = np.delete(distribution, index)
 	return distribution
-	
+
+def getConnStats(graphInput, fileName):
+	graphNodes = list(graphInput.nodes)
+	graphDegrees = list(graphInput.degree(graphNodes))
+	degrees = []
+	for x in graphDegrees:
+		degrees.append(x[1])
+	averageDegree = np.mean(degrees)
+	stdDeviation = np.std(degrees)
+	degreeCorrelation = nx.degree_assortativity_coefficient(graphInput)
+	fileName.write("--------------- Connectivity Statistics----------------\n")   
+	fileName.write("Average Degree:  " + str(averageDegree) + "\n") 
+	fileName.write("Standard Deviation:  " + str(stdDeviation) + "\n")
+	fileName.write("Degree Correlation:  " + str(degreeCorrelation) + "\n")
+	fileName.write("----------------Connectivity Statistics end -------------------\n")
+
+
+def getDistStats(graphInput, fileName):
+	avPathLength = nx.average_shortest_path_length(graphInput)
+	graphDiameter = nx.diameter(graphInput)
+	eccentricityList = nx.eccentricity(graphInput)
+	#print(eccentricityList.values())
+	averageEccentricity = np.mean(eccentricityList.values())
+	fileName.write("--------------- Distance Statistics----------------\n")   
+	fileName.write("Average Path Length:  " + str(avPathLength) + "\n") 
+	fileName.write("Graph Diameter:  " + str(graphDiameter) + "\n")
+	fileName.write("Average Eccentricity:  " + str(averageEccentricity) + "\n")
+	fileName.write("----------------Distance Statistics end -------------------\n")
+
+def getClustStats(graphInput, fileName):
+	triangles = nx.triangles(graphInput)
+	transitivity = nx.transitivity(graphInput)
+	aveClustCoeff = nx.average_clustering(graphInput)
+	fileName.write("--------------- Clustering Statistics----------------\n")   
+	fileName.write("Number of triangles:  " + str(triangles) + "\n") 
+	fileName.write("Graph Transitivity:  " + str(transitivity) + "\n")
+	fileName.write("Average Clustering Coefficient:  " + str(aveClustCoeff) + "\n")
+	fileName.write("----------------Clustering Statistics end -------------------\n")
+
+def getCentralStats(graphInput,fileName):
+	aveDegreeCentrality = np.mean(nx.degree_centrality(graphInput).values())
+	aveClosenessCentrality = np.mean(nx.closeness_centrality(graphInput).values())
+	aveBetweennessCentrality = np.mean(nx.betweenness_centrality(graphInput).values())
+	fileName.write("--------------- Centrality Statistics----------------\n")   
+	fileName.write("Average Degree Cenetrality:  " + str(aveDegreeCentrality) + "\n") 
+	fileName.write("Average Closeness Centrality:  " + str(aveClosenessCentrality) + "\n")
+	fileName.write("Average Betweenness Centrality:  " + str(aveBetweennessCentrality) + "\n")
+	fileName.write("----------------Centrality Statistics end -------------------\n")
+
+
 def showGraphInfo(graphInput,fileName):
     """ Show information on a given Graph """
     fileName.write("--------------- Graph Information----------------\n")   
@@ -98,9 +156,12 @@ def obtainGraphStats(graphIn, outputDir):
 	outFileName = outputDir + "/graphStats.txt"
 	outputStats = open(outFileName,'w')
 	showGraphInfo(graphIn, outputStats)
-	#showVertexInfo(graphInput,outputStats)
-	#getDegreeDist(graphInput, outputDir)
 	getHist(graphIn, outputDir)
+	netXGraph = convertGraph(graphIn)
+	getConnStats(netXGraph, outputStats)
+	getDistStats(netXGraph, outputStats)
+	getClustStats(netXGraph, outputStats)
+	getCentralStats(netXGraph, outputStats)
 	outputStats.close()
 
 
